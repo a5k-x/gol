f6197f254f77fcc1580cda8ce632882fbcea9e6b
Каштанов Александр Сергеевич <aleks-hd2015@yandex.ru>
2022-10-12 22:22:23 +0400

    add programm

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿String[] ert =  {"kot", "shmot","1", "bl", "hosting"};
+
+String[] newArray = getNewArray(ert);
+printArray(newArray);
+
+String[] getNewArray(String[] array){
+    String[] newArray = new String[array.Length];
+    int count = 0;
+    for(int i = 0; i < array.Length; i++){
+        if (array[i].Length <= 3 ){
+            newArray[count] = array[i];
+            count++;
+        }
+    }
+    Array.Resize<String>(ref newArray, count);
+    return newArray;
+}
+
+void printArray(String[] array){
+   Console.WriteLine(array.Length + " Size ");
+    foreach(String i in array){
+        
+        Console.Write(i + "; ");
+    }
+}
